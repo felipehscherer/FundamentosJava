2293ea7efab2dd65acfb610c432bc697e56352fd
Felipe H. Scherer <felipe.scherepento@gmail.com>
2024-03-25 17:02:18 -0300

    Finalização do curso

--- ProjetoStreams/src/main/java/br/com/felipehs/Main.java
+++ ProjetoStreams/src/main/java/br/com/felipehs/Main.java
@@ -12,12 +12,20 @@ public class Main {
         lista.add(new Produto("Mouse", 15, 98.90));
         lista.add(new Produto("Teclado", 20, 139.90));
         lista.add(new Produto("Monitor", 50, 799));
-        lista.add(new Produto("MousePad", 20, 27.90));
+        lista.add(new Produto("MousePad", 200, 27.90));
 
         System.out.println("Produtos com estoque grande(p >= 100)");
         lista.stream().filter(produto -> produto.getQuantiaEmEstoque() >= 100)
                 .forEach(System.out::println);
-
-
+        System.out.println("---------------------");
+        System.out.println("Produtos de até 50 reais");
+        lista.stream().filter(produto -> produto.getValor() <= 50)
+                .forEach(System.out::println);
+        System.out.println("---------------------");
+        System.out.println("Estoque");
+        double estoque;
+        estoque = lista.stream().map(produto -> produto.getQuantiaEmEstoque()).reduce(0, (q1, q2) -> q1 + q2);
+        System.out.println(estoque);
+        
     }
 }
